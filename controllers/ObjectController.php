0ac2ed3f2f572286639fe54d4cd0b672d854ef8a
grisha <grisha@DESKTOP-SSK063D>
2026-05-06 14:46:02 +0800

    круд 5.3 done

--- controllers/ObjectController.php
+++ controllers/ObjectController.php
@@ -6,25 +6,56 @@ class ObjectController extends BasePhoneTwigController {
     
     public function getContext(): array
     {
+        
         $context = parent::getContext();
         
+      
         $my_id = $this->params['my_id'] ?? $this->params[1] ?? 0;
         
-
+        $show = $_GET['show'] ?? null;
+        
         $query = $this->pdo->prepare("SELECT * FROM phone_objects WHERE id = :my_id");
         $query->execute(['my_id' => $my_id]);
         $object = $query->fetch();
         
-        if ($object) {
-            $context['title'] = $object['title'];
-            $context['url_title'] = "phone-object";
-            $context['my_id'] = $my_id;
-            $context['description'] = $object['description'] ?? "Нет описания";
-            $context['image'] = $object['image'] ?? "";
-        } else {
+        if (!$object) {
+            
             $context['title'] = "Объект не найден";
             $context['my_id'] = $my_id;
             $context['description'] = "Объект с ID {$my_id} не существует";
+            $context['is_infoActive'] = false;
+            $context['is_imgActive'] = false;
+            return $context;
+        }
+        
+      
+        $context['title'] = $object['title'];
+        $context['url_title'] = "phone-object";
+        $context['my_id'] = $my_id;
+        $context['image'] = $object['image'] ?? "";
+        $context['description'] = $object['description'] ?? "Нет описания";
+        $context['info'] = $object['info'] ?? "Нет полной информации";
+        
+
+        if ($show === 'image') {
+            $context['is_imgActive'] = true;
+            $context['is_infoActive'] = false;
+            $context['image_url'] = $object['image'] ?? "/images/placeholder.jpg";
+        
+            $this->template = "__object_image.twig";
+        }
+     
+        else if ($show === 'info') {
+            $context['is_imgActive'] = false;
+            $context['is_infoActive'] = true;
+         
+            $this->template = "__object_info.twig";
+        }
+        
+        else {
+            $context['is_imgActive'] = false;
+            $context['is_infoActive'] = false;
+           
         }
         
         return $context;
--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ require_once "../vendor/autoload.php";
 require_once "../framework/autoload.php";
 require_once "../controllers/MainController.php";
 require_once "../controllers/ObjectController.php";
-require_once "../controllers/ObjectImageController.php";
-require_once "../controllers/ObjectInfoController.php";
 require_once "../controllers/Controller404.php";
 
 $loader = new \Twig\Loader\FilesystemLoader("../views");
@@ -19,7 +17,5 @@ $pdo = new PDO("mysql:host=localhost;dbname=mobile_phone;charset=utf8", "root", 
 $router = new Router($twig, $pdo);
 $router->add("/", MainController::class);
 $router->add("/phone-object/(?P<my_id>\d+)", ObjectController::class);
-$router->add("/phone-object/(?P<my_id>\d+)/image", ObjectImageController::class);
-$router->add("/phone-object/(?P<my_id>\d+)/info", ObjectInfoController::class);
 
 $router->get_or_default(Controller404::class);
