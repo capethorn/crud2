6b3062cbfa5858a94e4f6645a79f10bf20ad02aa
grisha <grisha@DESKTOP-SSK063D>
2026-05-07 14:38:30 +0800

    поиск без задания

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -7,8 +7,26 @@ class SearchController extends BasePhoneTwigController {
     public function getContext(): array
     {
         $context = parent::getContext();
-
-
+        
+        $type = isset($_GET['type']) ? $_GET['type'] : '';
+        $title = isset($_GET['title']) ? $_GET['title'] : '';
+        
+        $sql = <<<EOL
+SELECT id, title
+FROM phone_objects
+WHERE (:type = '' OR type = :type)
+  AND (:title = '' OR title LIKE CONCAT('%', :title, '%'))
+EOL;
+        
+        $query = $this->pdo->prepare($sql);
+        $query->bindValue("type", $type);
+        $query->bindValue("title", $title);
+        $query->execute();
+        
+        $context['phone_objects'] = $query->fetchAll();
+        $context['selected_type'] = $type;
+        $context['search_title'] = $title;
+        
         return $context;
     }
 }
